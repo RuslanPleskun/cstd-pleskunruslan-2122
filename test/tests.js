a0f2a55e56bb86ff5321f17aa815f5f358dba83b
RuslanPleskun <ruslanpleskun5@gmail.com>
2021-11-28 17:57:09 +0200

    fix

--- test/tests.js
+++ test/tests.js
@@ -2,10 +2,8 @@ const {Builder, By, Key} = require('selenium-webdriver');
 const assert = require('assert');
 const should = require('chai').should();
 
-// describe docblock
 describe('Tests', function() {
-  // it block
-  it('first test', async function() {
+  it('second test', async function() {
     const driver = await new Builder().forBrowser('chrome').build();
 
     await driver.get('https://www.wikipedia.org/');
@@ -14,7 +12,7 @@ describe('Tests', function() {
 
     await driver.quit();
   });
-  it('second test', async function() {
+  it('first test', async function() {
     const driver = await new Builder().forBrowser('chrome').build();
 
     await driver.get('https://www.wikipedia.org/');
