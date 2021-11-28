1adc5898fc8be74808184eefccd8c6d403dd41e6
RuslanPleskun <ruslanpleskun5@gmail.com>
2021-11-28 18:25:22 +0200

    fixed

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,16 @@ const assert = require('assert');
 const should = require('chai').should();
 
 describe('Tests', function() {
-  it('test', async function() {
+  it('second test', async function() {
+    const driver = await new Builder().forBrowser('chrome').build();
+
+    await driver.get('https://www.wikipedia.org/');
+
+    await driver.findElement(By.id('searchInput')).sendKeys('The USA', Key.RETURN);
+
+    await driver.quit();
+  });
+  it('first test', async function() {
     const driver = await new Builder().forBrowser('chrome').build();
 
     await driver.get('https://www.wikipedia.org/');
