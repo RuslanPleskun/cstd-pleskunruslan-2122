8c5a923b0277f71eb8fca7e1084bf1d38e63fe86
RuslanPleskun <ruslanpleskun5@gmail.com>
2021-11-29 12:22:09 +0200

    fix

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const should = require('chai').should();
 
 describe('Tests', function() {
-  it('first test', async function() {
+  it('Page Navigation', async function() {
     const driver = await new Builder().forBrowser('chrome').build();
     //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     await driver.manage().window().maximize();
@@ -12,6 +12,12 @@ describe('Tests', function() {
     //await driver.sleep(3000);
     await driver.findElement(By.id('season')).click();
     await driver.sleep(3000);
+    // await driver.findElement(By.css('section:nth-of-type(6) > .order-button')).click();
+    // await driver.sleep(3000);
+    // await driver.findElement(By.xpath("//a[@href='https://rickandmorty.fandom.com/wiki/Rickipedia']")).click();
+    // await driver.sleep(3000);
+    // await driver.findElement(By.css('video')).click();
+    // await driver.sleep(3000);
 //    await driver.findElement(By.xpath("//a[@href='#seasons']")).click();
 
     await driver.quit();
